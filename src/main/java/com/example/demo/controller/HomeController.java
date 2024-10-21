b9a29c07ddf4868a4a587b62b6b83f68c6dd0d2d
Jason Lee <jason987321z@gmail.com>
2024-10-21 18:47:04 +0800

    新增討論區頁面。 navBar bootstrap下拉選單問題解決, 可能是bootstrap5 conflict with jquery

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,36 +9,53 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/home")
 public class HomeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
 
-	
 	// 首頁
 	@GetMapping("/index")
 	public String index() {
 		log.info("return index page");
 		return "view/index";
 	}
-	
-	
+
 	@GetMapping("/signup")
 	public String signup() {
 		log.info("return signup page");
 		return "view/signup";
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
 		log.info("return login page");
 		return "view/login";
 	}
-	
+
 	@GetMapping("/allArticle")
 	public String article() {
 		log.info("return allArticle page");
 		return "view/allArticle";
 	}
 
-	
-	
+	@GetMapping("/navbar")
+	public String navbar() {
+		log.info("return navbar ");
+		return "view/navbar";
+	}
+
+	/********************** 購車討論區 ***********************************/
+	// 購車-新車
+	@GetMapping("/buyNewCar")
+	public String buyNewCar() {
+		log.info("return buyNewCar ");
+		return "view/buyCarArea/buyNewCar";
+	}
+
+	// 購車-新車
+	@GetMapping("/buyUsedCar")
+	public String buyUsedCar() {
+		log.info("return buyUsedCar ");
+		return "view/buyCarArea/buyUsedCar";
+	}
+
 }
